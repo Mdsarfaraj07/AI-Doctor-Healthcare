867a41b3518694668665d29c67d206d172a9c02e
Mdsarfaraj07 <mdsarfaraj9886@gmail.com>
2026-08-11 12:35:57 +0530

    Add Render deployment configuration

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from flask import Flask, request, render_template, jsonify  # Import jsonify
 import numpy as np
 import pandas as pd
 import pickle
+import os
 
 
 # flask app
@@ -128,5 +129,5 @@ def blog():
 
 
 if __name__ == '__main__':
-
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
